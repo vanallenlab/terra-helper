2b046760e709189e21bb40ebc55d7300bb0a8aa3
Brendan Reardon <breardon@broadinstitute.org>
2021-03-30 17:57:24 -0400

    Added row to show costs to stay in Terra

--- estimate_archive_and_retrieval_cost.py
+++ estimate_archive_and_retrieval_cost.py
@@ -1,6 +1,8 @@
 import argparse
 import pandas as pd
+import sys
 import index_workspace
+from google.cloud import storage
 
 root = 'https://api.firecloud.org/api'
 
@@ -10,6 +12,7 @@ root = 'https://api.firecloud.org/api'
 
 US = 'us'
 US_CENTRAL = 'us-central1'
+TERRA = 'stay in terra'
 STANDARD = 'standard'
 NEARLINE = 'nearline'
 COLDLINE = 'coldline'
@@ -101,13 +104,29 @@ def adjust_storage_charge_fees(time_scale, dataframe, fees_breakdown):
     return df
 
 
-def calculate_fees(network, a_operations, retrieval):
+def append_row_for_terra(dataframe, bucket_location, set_to_zero=False):
+    terra_row = (
+        dataframe
+        .loc[(bucket_location, STANDARD), dataframe.columns]
+        .rename((bucket_location, TERRA))
+        .to_frame()
+        .T
+        .reset_index()
+        .rename(columns={'level_0': 'location', 'level_1': 'storage_type'})
+        .set_index(['location', 'storage_type'])
+    )
+    if set_to_zero:
+        terra_row.loc[:] = 0
+    return pd.concat([dataframe, terra_row])
+
+
+def calculate_fees(network, a_operations, retrieval, bucket_location):
     # fees to archive from multi regional standard = network_to + operations_to
     # fees to retrieve from multi regional standard = network_from + operations_from + retrieval
     fees_dataframes = []
     for location in LOCATIONS:
-        archive_fees_network = network[US][location]  # Cost to archive from Terra (multi regional)
-        retrieval_fees_network = network[location][US]  # Cost to retrieve from location to Terra
+        archive_fees_network = network[bucket_location][location]  # Cost to archive from Terra
+        retrieval_fees_network = network[location][bucket_location]  # Cost to retrieve from location to Terra
         for storage_type in STORAGE_TYPES:
             archive_fees_a_operations = a_operations[storage_type]  # Cost to archive to new storage type
             retrieval_fees_a_operations = a_operations[STANDARD]  # Cost to retrieve to Terra (standard)
@@ -125,7 +144,7 @@ def calculate_fees(network, a_operations, retrieval):
             series.loc['archive_fee_total'] = archive_total
             series.loc['retrieval_fee_total'] = retrieval_total
             series.loc['fees_total'] = archive_total + retrieval_total
-            if location == US and storage_type == STANDARD:
+            if storage_type == TERRA:
                 series.loc[:] = 0
             series.loc['location'] = location
             series.loc['storage_type'] = storage_type
@@ -173,14 +192,14 @@ def calculate_storage_charges(usage_binary_gigabytes):
     return costs
 
 
-def calculate_storage_charges_over_time(time_scale, storage):
+def calculate_storage_charges_over_time(time_scale, storage_charges):
     assert time_scale in ['day', 'month'], 'time_scale must be day or month'
     divisor = 30 if time_scale == 'day' else 1
     duration = 550 if time_scale == 'day' else 19
     unit_label_column = f'{time_scale} storage cost'
     units = list(range(1, duration + 1))
 
-    df = pd.DataFrame(storage, columns=['location', 'storage_type', unit_label_column])
+    df = pd.DataFrame(storage_charges, columns=['location', 'storage_type', unit_label_column])
     if time_scale == 'day':
         df.loc[df.index, unit_label_column] = df.loc[df.index, unit_label_column] / divisor
     df.set_index(['location', 'storage_type'], inplace=True)
@@ -224,10 +243,12 @@ def calculate_usage_monthly_charges(usage_gigabytes):
     return usage_gigabytes * 0.026
 
 
-def create_page_information(dictionary, n_blobs, n_blobs_no_logs):
+def create_page_information(dictionary, n_blobs, n_blobs_no_logs, location):
     series = pd.Series(dictionary)
     series.loc['number of files'] = n_blobs
     series.loc['number of files, no logs'] = n_blobs_no_logs
+    series.loc['location_type'] = location['location_type']
+    series.loc['location'] = location['location']
     return series
 
 
@@ -247,7 +268,7 @@ def create_page_recommendations(dataframe):
     dataframe.columns = [str(column) for column in dataframe.columns]
     for day in ['7', '15', '30', '45', '60', '90', '120', '150', '180', '270', '360', '540']:
         location, storage_type, cumulative_cost = extract_recommendation(dataframe, day)
-        terra_cost = dataframe.loc[(US, STANDARD), day]
+        terra_cost = dataframe.loc[(US, TERRA), day]
         savings = cumulative_cost - terra_cost
         dictionary = {'days archived': day,
                       'recommended location': location,
@@ -279,6 +300,20 @@ def index_bucket(bucket_name):
     blobs = index_workspace.glob_bucket(bucket_name)
     blobs_no_logs = index_workspace.remove_logs_from_blobs(blobs)
     return blobs, blobs_no_logs
+
+
+def get_bucket_location(bucket_name):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    bucket_location_type = bucket.location_type
+    bucket_location = bucket.location
+    if bucket_location == 'US':
+        return {'location_type': bucket_location_type, 'location': US}
+    elif bucket_location == 'US-CENTRAL1':
+        return {'location_type': bucket_location_type, 'location': US_CENTRAL}
+    else:
+        print(bucket_location)
+        sys.exit('oh dear, a different location.')
 
 
 def request_workspace(namespace, name):
@@ -324,6 +359,7 @@ if __name__ == "__main__":
     print_enabled = args.print
 
     workspace = request_workspace(input_namespace, input_name)
+    location = get_bucket_location(workspace['bucket_name'])
 
     storage_costs = calculate_storage_charges(workspace['usage_binary_gigabytes'])
     network_costs = calculate_network_charges(workspace['usage_binary_gigabytes'])
@@ -332,14 +368,19 @@ if __name__ == "__main__":
     storage_costs_monthly = calculate_storage_charges_over_time('month', storage_costs)
     storage_costs_daily = calculate_storage_charges_over_time('day', storage_costs)
 
+    # Create rows for current storage in Terra
+    storage_costs_monthly_with_terra = append_row_for_terra(storage_costs_monthly, location['location'])
+    storage_costs_daily_with_terra = append_row_for_terra(storage_costs_daily, location['location'])
+
     blobs, blobs_no_logs = index_bucket(workspace['bucket_name'])
     class_a_operation_costs = calculate_operations_charges(len(blobs), OPERATION_A_COST)
     class_a_operation_costs_no_logs = calculate_operations_charges(len(blobs_no_logs), OPERATION_A_COST)
-    fees = calculate_fees(network_costs, class_a_operation_costs, retrieval_costs)
+    fees = calculate_fees(network_costs, class_a_operation_costs, retrieval_costs, location['location'])
+    fees_with_terra = append_row_for_terra(fees, location['location'], set_to_zero=True)
 
     # We adjust daily storage prices for minimum storage duration and then also add fees of archiving and retrieval
-    storage_costs_daily_min_duration = adjust_daily_storage_charge(storage_costs_daily)
-    storage_costs_daily_with_fees = adjust_storage_charge_fees('day', storage_costs_daily_min_duration, fees)
+    storage_costs_daily_min_duration = adjust_daily_storage_charge(storage_costs_daily_with_terra)
+    storage_costs_daily_with_fees = adjust_storage_charge_fees('day', storage_costs_daily_min_duration, fees_with_terra)
 
     if print_enabled:
         print(workspace)
@@ -350,7 +391,7 @@ if __name__ == "__main__":
         print(len(blobs_no_logs), class_a_operation_costs_no_logs)
         print(fees)
 
-    page_information = create_page_information(workspace, len(blobs), len(blobs_no_logs))
+    page_information = create_page_information(workspace, len(blobs), len(blobs_no_logs), location)
     page_recommendations = create_page_recommendations(storage_costs_daily_with_fees)
     page_intersections = calculate_intersections(storage_costs_daily_with_fees)
     page_storage = create_page_storage_costs(storage_costs)
@@ -361,7 +402,7 @@ if __name__ == "__main__":
                                                            len(blobs_no_logs)
                                                            )
     page_retrieval = create_page_retrieval_costs(retrieval_costs)
-    page_fees = fees.copy(deep=True)
+    page_fees = fees_with_terra.copy(deep=True)
 
     output_name = f'{input_namespace}.{input_name}.cost-estimates'
     with pd.ExcelWriter(f'{output_name}.full.xlsx') as writer:
@@ -373,8 +414,8 @@ if __name__ == "__main__":
         page_class_a_operations.to_excel(writer, sheet_name='Class A operations gsutil cp')
         page_retrieval.to_excel(writer, sheet_name='Retrieval costs', header=False)
         page_fees.to_excel(writer, sheet_name='Fees')
-        storage_costs_monthly.to_excel(writer, sheet_name='monthly storage cost')
-        storage_costs_daily.to_excel(writer, sheet_name='daily storage cost')
+        storage_costs_monthly_with_terra.to_excel(writer, sheet_name='monthly storage cost')
+        storage_costs_daily_with_terra.to_excel(writer, sheet_name='daily storage cost')
         storage_costs_daily_min_duration.to_excel(writer, sheet_name='daily, adj. min duration')
         storage_costs_daily_with_fees.to_excel(writer, sheet_name='daily adj min duration w fees')
 
